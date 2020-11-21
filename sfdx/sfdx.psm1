2db9b48c833ddace7b6f44fff2e4306a93ee934c
Chirag Paliwal <chirag.paliwal06@gmail.com>
2020-11-21 17:15:45 +0530

    Add files via upload

---
+++ sfdx/sfdx.psm1
@@ -0,0 +1,15 @@
+﻿function dxpush {
+    sfdx force:source:push
+}
+
+function dxpush {
+    sfdx force:source:pull
+}
+
+function dxpush {
+    sfdx force:org:open
+}
+
+function dxlist {
+    sfdx force:org:list
+}
